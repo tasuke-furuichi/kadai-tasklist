1417494f0c5b2e7345d22e49e431a40ff47df3eb
TASUKE <tasuke_0821@yahoo.co.jp>
2017-07-18 09:45:49 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,52 @@
+class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      flash[:success]="タスクを作成しました"
+      redirect_to @task
+    else
+      flash.now[:danger]="タスク作成に失敗しました"
+      render :new
+    end
+  end
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.save
+      flash[:success]="タスクを編集しました"
+      redirect_to @task
+    else
+      flash.now[:danger]="タスクを編集できませんでした"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    flash[:success]="タスクを削除しました"
+    redirect_to tasks_url
+  end
+    
+  private
+  def task_params
+    params.require(:task).permit(:content)
+  end
+  
+end
